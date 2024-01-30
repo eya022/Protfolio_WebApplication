a6ca5d41da0241b924b6e0a3e4c8ea36e1b53f7f
eya022 <eya.jemai@yahoo.com>
2024-01-30 21:35:23 +0100

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Skills } from './components/Skills';
+import {Projects} from './components/Projects'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar />
+    <Banner />
+    <Skills />
+    <Projects />
     </div>
   );
 }
